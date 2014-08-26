17c1e9f0733b6657bfd8d773fc7149e61f4a1623
azqazq1234 <flower89757@gmail.com>
2014-08-26 23:09:15 +0800

    jq

--- script/unity.js
+++ script/unity.js
@@ -6,27 +6,4 @@ $(function(){
 	$('section section img').mouseleave(function(){
 		$(this).animate({width:'80%'},500).css("border","0px")
 	})
-
-	$('nav.nav_title li').mouseenter(function(){
-		$('nav.nav_title li ul').slideDown(500)
-	})
-	$('nav.nav_title li').mouseleave(function(){
-		$('nav.nav_title li ul').slideUp(500)
-	})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
-
