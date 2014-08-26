c991bfae3c245df787f5c86a6829e779628ee59c
azqazq1234 <flower89757@gmail.com>
2014-08-26 22:31:46 +0800

    q

--- script/unity.js
+++ script/unity.js
@@ -1,8 +1,32 @@
 $(function(){
 	$('section section img').mouseenter(function(){
-		$(this).animate({width:'90%'},500).css({"border":"5px solid black","borderRadius":"5px","borderColor":"grey"})
+		$(this).animate({width:'90%'},500)
+		.css({"border":"5px solid black","borderRadius":"5px","borderColor":"grey"})
 	})
 	$('section section img').mouseleave(function(){
 		$(this).animate({width:'80%'},500).css("border","0px")
 	})
+
+	$('nav.nav_title li').mouseenter(function(){
+		$('nav.nav_title li ul').slideDown(500)
+	})
+	$('nav.nav_title li').mouseleave(function(){
+		$('nav.nav_title li ul').slideUp(500)
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
+
