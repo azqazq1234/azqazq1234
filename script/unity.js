089a37d2282acaa852352df79812fe53b7cc320f
azqazq1234 <flower89757@gmail.com>
2014-08-26 22:00:39 +0800

    jq

---
+++ script/unity.js
@@ -0,0 +1,8 @@
+$(function(){
+	$('section section img').mouseenter(function(){
+		$(this).animate({width:'90%'},500).css({"border":"5px solid black","borderRadius":"5px","borderColor":"grey"})
+	})
+	$('section section img').mouseleave(function(){
+		$(this).animate({width:'80%'},500).css("border","0px")
+	})
+})
